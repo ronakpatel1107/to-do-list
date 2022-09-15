2768c6ac8255753b9051af9bab9041d35b1fd3c7
ronakpatel1107 <104769863+ronakpatel1107@users.noreply.github.com>
2022-09-15 14:31:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {
+        item:"",
+        
+      status: false
+      }, 
+      users : [],
+      pos:-1,
+    };
+    
+  }
+  
+  handleChange =(e)=>{
+    console.log(e.target.name)
+    const {name, value} = e.target;
+    let user = {...this.state.user};
+    user[name] = value;
+
+    this.setState({
+      user,
+    },()=> console.log(user));
+
+  }
+
+  clearForm = () => {
+    this.setState({
+      user: {
+        item:"",
+        status: false
+      },
+    });
+  }
+
+  handleSave = (index, remove) =>{
+    let users = [...this.state.users];
+    console.log(users)
+    this.clearForm();
+
+    if(index>-1 && remove)
+      users[index].status=!users[index].status
+    else if(index>-1 && !remove)
+      users.splice(index,1)
+    else if(index==-1)
+      users.push(this.state.user);
+    else if(index>-1)
+      users[index] = this.state.user;
+
+    console.log("users", users);
+    this.setState({
+      users,
+      pos : -1,
+    });
+  }
+
+  handleQuickEdit = (index) => {
+    let users = [...this.state.users];
+    this.setState({
+      user : users[index],
+      pos : index
+    });
+  }
+
+
+  render(){
+    return(
+      <div className='App'>
+          <form>
+            <h1>To Do List</h1>
+            <label htmlFor="name"> Item  : </label>
+            <input type="text" name="item" value={this.state.user.item} onChange={(e)=>{this.handleChange(e)}}/><br/><br/>
+            <input type="button" value="Submit" onClick={()=>{this.handleSave(this.state.pos)}}/>   <br/><br/> 
+          </form>
+
+          <table>
+            <thead>
+              <tr>
+                <td>List</td>
+                <td>Action</td>
+              </tr>
+            </thead>
+            <tbody>
+            {this.state.users.length 
+              ?
+                this.state.users.map((ele, index)=>{
+                  return (
+                      <tr key={index}>
+                        <td><span style={{textDecoration:(ele.status?'line-through':'')}}>{ele.item}</span></td>
+                        <td>
+                          <div>
+                            {ele.status?
+                              null:<button onClick={()=>{this.handleQuickEdit(index)}}>Edit</button>}
+
+                              <button onClick={()=>{this.handleSave(index, true)}}>{ele.status?'Complete':'Pending'}</button>
+                              <button onClick={()=>{this.handleSave(index, false)}}>Delete</button>
+                          </div>
+                        </td>
+                      </tr>
+                  )
+                })
+              :
+                <tr>No data exists</tr>
+            }
+            </tbody>
+          </table>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
